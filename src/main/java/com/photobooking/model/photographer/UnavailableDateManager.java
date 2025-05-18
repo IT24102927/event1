450b318aa5d92f1026c5d6cd904d4da5c2458d99
Hirusha <it24102740@my.sliit.lk>
2025-05-18 10:24:55 +0530

    create BlockDatesServlet.java and UnavailableDateManager.java

--- src/main/java/com/photobooking/model/photographer/UnavailableDateManager.java
+++ src/main/java/com/photobooking/model/photographer/UnavailableDateManager.java
@@ -8,7 +8,7 @@ import java.util.logging.Logger;
 import java.util.logging.Level;
 import java.util.stream.Collectors;
 
-/**
+/*
  * Manages photographer unavailable dates
  */
 public class UnavailableDateManager {
